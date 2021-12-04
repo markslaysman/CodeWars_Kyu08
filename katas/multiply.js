56f9f58c56b7a22567d1ade8b2855f1b4d8bae3b
Mark D. Slaysman <mark_slaysman@yahoo.com>
2021-12-03 19:32:07 -0700

    Original Solution to Multiply.

---
+++ katas/multiply.js
@@ -0,0 +1,33 @@
+/*
+
+https://www.codewars.com/kata/50654ddff44f800200000004
+
+This code does not execute properly. Try to figure out why.
+
+function multiply(a, b){
+  a * b
+}
+
+
+
+Original Solve Date: 11/12/2021
+*/
+
+function multiply(a, b){
+    return (a * b);
+}
+
+
+// Sample Data
+
+// Should return 1
+console.log(multiply(1,1));
+
+// Should return 2
+console.log(multiply(2,1));
+
+// Should return 4
+console.log(multiply(2,2));
+
+// Should return 15
+console.log(multiply(3,5));
